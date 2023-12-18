3f4020d40c83f7436b7b27ed6051aabe425c41b3
ROBERTO MATHEUS LIMA TORRES <07121450526@pmse.gov>
2023-12-18 09:27:42 -0300

    crinado page de completar ordem e ordem confirmada

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,13 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import { DefaultLayout } from "./layouts/DefaultLayout";
 import { Home } from "./pages/Home";
+import { CompleteOrder } from "./pages/CompleteOrder";
+import { OrderConfirmed } from "./pages/OrderConfirmed";
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
-        
+        <Route path="/completeorder" element={<CompleteOrder />} />
+        <Route path="/orderconfirmed" element={<OrderConfirmed />} />
       </Route>
     </Routes>
   );
